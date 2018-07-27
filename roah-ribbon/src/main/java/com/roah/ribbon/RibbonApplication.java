f7c61726537dc5757e8bde373da1e10266cfa390
RuanHang <ruanhang@missfresh.cn>
2018-07-27 16:27:03 +0800

    ribbon,ribbon

---
+++ roah-ribbon/src/main/java/com/roah/ribbon/RibbonApplication.java
@@ -0,0 +1,12 @@
+package com.roah.ribbon;
+
+/**
+ * TODO: Add Description
+ *
+ * @author Roah
+ * @since 07/27/2018
+ */
+public class RibbonApplication {
+
+
+}
---
+++ roah-service/src/main/java/com/roah/service/TestService.java
@@ -0,0 +1,11 @@
+package com.roah.service;
+
+/**
+ * TODO: Add Description
+ *
+ * @author Roah
+ * @since 07/27/2018
+ */
+public interface TestService {
+    public String test(String test);
+}
