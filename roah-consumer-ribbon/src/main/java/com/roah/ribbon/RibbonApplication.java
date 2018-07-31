a62f683df177ff7762cdb3af5e3d1c1bebf5168e
RuanHang <ruanhang@missfresh.cn>
2018-07-31 18:16:50 +0800

    基础配置完善

---
+++ roah-consumer-ribbon/src/main/java/com/roah/ribbon/RibbonApplication.java
@@ -0,0 +1,25 @@
+package com.roah.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * TODO: Add Description
+ *
+ * @author Roah
+ * @since 07/27/2018
+ */
+@SpringBootApplication
+public class RibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class,args);
+    }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
