da185fe8099f90e216b77a3bc6608373406c3270
RuanHang <ruanhang@missfresh.cn>
2018-08-03 11:52:22 +0800

    Hystrix监控

---
+++ roah-hystrix/src/main/java/com/roah/turbine/TurbineApplication.java
@@ -0,0 +1,31 @@
+package com.roah.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO: Add Description
+ *
+ * @author Roah
+ * @since 08/03/2018
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
+public class TurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class,args);
+    }
+}
